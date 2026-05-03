b8eb7198d4cad61d17b72c5593ab0020d085576c
Tisha Dey <tishadey2025@outlook.com>
2026-05-03 13:00:27 +0600

    :tada: navbar and footer design done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
 export default function Footer() {
   return (
-    <div className="max-w-4xl mx-auto py-10 px-6">
-      <h1 className="text-4xl font-bold text-center mb-6">This is Footer</h1>
-    </div>
+    <footer className="bg-green-950 text-white p-10">
+      <div className="footer sm:footer-horizontal">
+        <nav>
+          <h6 className="footer-title">Contact Info</h6>
+          <p>Email: support@qurbanihat.com</p>
+          <p>Phone: +880 1234-567890</p>
+          <p>Address: Barishal, Bangladesh</p>
+        </nav>
+
+        <nav>
+          <h6 className="footer-title">Social Links</h6>
+          <a className="flex items-center gap-2 link link-hover">Facebook</a>
+          <a className="flex items-center gap-2 link link-hover">Twitter</a>
+          <a className="flex items-center gap-2 link link-hover">Instagram</a>
+          <a className="flex items-center gap-2 link link-hover">LinkedIn</a>
+        </nav>
+
+        <nav>
+          <h6 className="footer-title">About</h6>
+          <p className="max-w-xs">
+            QurbaniHat is an online marketplace where you can easily buy and
+            sell qurbani animals.
+          </p>
+        </nav>
+      </div>
+      <div className="w-full text-center mt-6 border-t border-gray-600 pt-4">
+        <p>© {new Date().getFullYear()} QurbaniHat. All rights reserved.</p>
+      </div>
+    </footer>
   );
 }
