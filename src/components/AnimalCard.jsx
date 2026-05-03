1fc71cc8a0bda0add210a53c00f79773c3c14d85
Tisha Dey <tishadey2025@outlook.com>
2026-05-04 01:12:42 +0600

    :green_heart: data json updated

--- src/components/AnimalCard.jsx
+++ src/components/AnimalCard.jsx
@@ -5,12 +5,12 @@ export default async function AnimalCard () {
     `https://raw.githubusercontent.com/tishadey45/qurbani-hat/refs/heads/main/data.json`,
   );
   const data = await res.json();
-  const topPhotos = data.slice(0, 4);
+  const topAnimals = data.slice(0, 4);
   return (
     <div>
       <h1 className="text-4xl font-bold text-center mb-6">Featured animals</h1>
       <div className="grid grid-cols-2 gap-6">
-        {topPhotos.map((animal) => (
+        {topAnimals.map((animal) => (
           <ImageCard key={animal.id} animal={animal} />
         ))}
       </div>
