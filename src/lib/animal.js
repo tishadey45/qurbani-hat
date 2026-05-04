81a464d44418b2eb87d6e18083ab2fdd4dbad8f1
Tisha Dey <tishadey2025@outlook.com>
2026-05-04 12:41:43 +0600

    :art: api link updated

--- src/lib/animal.js
+++ src/lib/animal.js
@@ -1,4 +1,4 @@
 export const allAnimals = async()=>{
-const res =await fetch(`https://raw.githubusercontent.com/tishadey45/qurbani-hat/refs/heads/main/public/data.json`);
+const res =await fetch(`https://qurbani-hat-beta.vercel.app/data.json`);
   return res.json();
 }
