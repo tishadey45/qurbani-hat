118970012fcd2dc394112d3863f07bfefd60a740
Tisha Dey <tishadey2025@outlook.com>
2026-05-04 23:04:15 +0600

    :tada: private route implemented

---
+++ src/proxy.js
@@ -0,0 +1,17 @@
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/profile", "/all-animals/:path"],
+};
